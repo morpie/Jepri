fb45dd311dc668dd031ddec7d89c8b01c767b772
morpie <morpie15@gmail.com>
2015-07-06 01:54:24 +0200

    #2 Feature implemented. Added storage in the database of project_name,project_id and project_creation_date. All stored in table project_info

--- bin/jpr_init.sh
+++ bin/jpr_init.sh
@@ -20,3 +20,14 @@ mkdir data
 sqlite3 $PROJECT_HOME/.jepri/data/database.db 'CREATE TABLE project_info (key TEXT,value TEXT)'
 sqlite3 $PROJECT_HOME/.jepri/data/database.db 'CREATE TABLE tickets (ID INTEGER PRIMARY KEY,description TEXT,owner TEXT, open_version TEXT, delivered_version TEXT, type TEXT, status TEXT, links TEXT, history TEXT, attachments TEXT)'
 
+echo "Enter the project ID (no spaces)"
+read PROJECT_ID
+
+PROJECT_CREATION_DATE=$(date)
+
+sqlite3 $PROJECT_HOME/.jepri/data/database.db << EOS
+INSERT INTO project_info (key,value) VALUES ('project_name','$1');
+INSERT INTO project_info (key,value) VALUES ('project_id','$PROJECT_ID');
+INSERT INTO project_info (key,value) VALUES ('project_creation_date','$PROJECT_CREATION_DATE');
+EOS
+
