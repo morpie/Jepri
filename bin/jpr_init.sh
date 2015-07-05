12c42cb57c78a30520f54b465044d8a740e24c55
morpie <morpie15@gmail.com>
2015-07-06 01:38:09 +0200

    Basic init script created. Scripts needs the name of the project passed as argument, if not given print usage mode and exits. It creates a folder with the project name and a .jepri folder inside with a database.

--- bin/jpr_init.sh
+++ bin/jpr_init.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-# Empty 
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage:"
+	echo "pass as argument the path/projectName"
+	exit
+fi
+
+# Creating the file tree
+mkdir $1
+cd $1
+PROJECT_HOME=$(pwd)
+
+mkdir .jepri
+cd .jepri
+mkdir data
+
+#creating database
+sqlite3 $PROJECT_HOME/.jepri/data/database.db 'CREATE TABLE project_info (key TEXT,value TEXT)'
+sqlite3 $PROJECT_HOME/.jepri/data/database.db 'CREATE TABLE tickets (ID INTEGER PRIMARY KEY,description TEXT,owner TEXT, open_version TEXT, delivered_version TEXT, type TEXT, status TEXT, links TEXT, history TEXT, attachments TEXT)'
+
